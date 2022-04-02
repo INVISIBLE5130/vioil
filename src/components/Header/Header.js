c29e14a579766602ce9cffa15bbbf9b083be052c
INVISIBLE5130 <goshakovan14@gmail.com>
2022-04-02 11:08:03 +0300

    Updated styles

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,16 +4,22 @@ import {Link} from "react-router-dom";
 import './Header.css'
 
 function Header() {
+    const activeTab = tab => window.location.pathname === tab
+        ? {
+            color: '#78C151'
+        }
+        : null
+
     return (
         <header className={'header'}>
-            <Link to={'/'} className={'header-link'}>
+            <Link to={'/'} className={'header-link'} style={activeTab('/')}>
                 Про товариство
             </Link>
-            <Link to={'/history'} className={'header-link'}>
+            <Link to={'/history'} className={'header-link'} style={activeTab('/history')}>
                 Історія
             </Link>
             <img className={'header-logo'} src={LogoSVG} alt="Logo"/>
-            <Link to={'/agroholding'} className={'header-link'}>
+            <Link to={'/agroholding'} className={'header-link'} style={activeTab('/agroholding')}>
                 Агрохолдинг
             </Link>
             <Link to={'/auth'} className={'header-link button-link'}>
